6a0dddc6e4d47a1c9bde9657089d01fffe774147
mridul <mridul.choudhary7@gmail.com>
2022-06-30 23:26:27 +0530

    Setup initial project structure

---
+++ src/main/java/com/wannabe/HelloWorld/Controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.wannabe.HelloWorld.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class IndexController {
+
+  @GetMapping("/v1/greet")
+  public ResponseEntity<?> greeting() {
+    return ResponseEntity.ok("Request success !");
+  }
+
+  @GetMapping("/v2/greet")
+  public ResponseEntity<?> greetingV2() {
+
+    return ResponseEntity.ok("Request v2 success !");
+  }
+}
