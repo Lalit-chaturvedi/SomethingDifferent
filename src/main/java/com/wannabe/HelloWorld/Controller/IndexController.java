243556b3b8c7bf50ca8fd1d8721859b557971488
Lalit Chaturvedi <72681015+Lalit-chaturvedi@users.noreply.github.com>
2023-06-24 16:59:54 +0530

    Update IndexController.java

--- src/main/java/com/wannabe/HelloWorld/Controller/IndexController.java
+++ src/main/java/com/wannabe/HelloWorld/Controller/IndexController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class IndexController {
 
+
   @GetMapping("/v1/greet")
   public ResponseEntity<?> greeting() {
     return ResponseEntity.ok("Request success !");
